f79f2a102afdadc0df3fda0be09db0c7dc4c3a7f
Muhammadkamran4616 <2504616@students.au.edu.pk>
2026-05-26 01:11:46 +0500

    Initial project setup

---
+++ RestaurantManagementSystem.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantManagementSystem.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ RestaurantManagementSystem.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantManagementSystem.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<MenuItem> MenuItems { get; set; }
+        = new List<MenuItem>();
+}
