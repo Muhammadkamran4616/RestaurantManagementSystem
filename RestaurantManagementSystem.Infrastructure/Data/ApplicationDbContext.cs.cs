17202b964c0292db584145b88e3737a2ff5ce213
Muhammadkamran4616 <2504616@students.au.edu.pk>
2026-05-26 13:36:23 +0500

    Setup EF Core with SQLite and Initial Migration

---
+++ RestaurantManagementSystem.Infrastructure/Data/ApplicationDbContext.cs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantManagementSystem.Domain.Entities;
+
+namespace RestaurantManagementSystem.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories => Set<Category>();
+
+    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+}
